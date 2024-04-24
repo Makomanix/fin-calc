c61e75456a0879f8753cc6c2cbfad10d5faa4511
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-04-24 16:24:52 -0400

    added htmlFor attributes in UserInput

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -6,26 +6,22 @@ export default function UserInput({inputs, handleChange}) {
         <section id="user-input">
             <div className="input-group">
                 <p>
-                    <label>Initial Investment
-                        <input type="number" name="initialInvestment" onChange={(e) => handleChange(e)} required></input>
-                    </label>
+                    <label htmlFor="initial" id="initial">Initial Investment</label>
+                    <input id="initial" type="number" name="initialInvestment" onChange={(e) => handleChange(e)} required></input>
                 </p>
                 <p>
-                    <label>Annual Investment
-                        <input type="number" name="annualInvestment" onChange={(e) => handleChange(e)} required></input>
-                    </label>
+                    <label htmlFor="annual" >Annual Investment</label>
+                    <input id="annual" type="number" name="annualInvestment" onChange={(e) => handleChange(e)} required></input>
                 </p>
             </div>
             <div className="input-group">
                 <p>
-                    <label>Expected Return
-                        <input type="number" name="expectedReturn" onChange={(e) => handleChange(e)} required></input>
-                    </label>
+                    <label htmlFor="expected">Expected Return</label>
+                    <input id="expected" type="number" name="expectedReturn" onChange={(e) => handleChange(e)} required></input>
                 </p>
                 <p>
-                    <label>Duration
-                        <input type="number" name="duration" onChange={(e) => handleChange(e)} required></input>
-                    </label>
+                    <label htmlFor="duration">Duration</label>
+                    <input id="duration"  type="number" name="duration" onChange={(e) => handleChange(e)} required></input>
                 </p>
             </div>
         </section>
